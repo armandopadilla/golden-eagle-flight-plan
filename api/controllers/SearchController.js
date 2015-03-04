57eda5ab155c638d2a1459c632cc55756c59d1a1
Armando Padilla <armando@crowdrise.com>
2015-03-04 23:29:40 +0000

    removing display of admin and advisors on teh search results

--- api/controllers/SearchController.js
+++ api/controllers/SearchController.js
@@ -1,35 +1,35 @@
 var validator = require("validator");
 
 module.exports = {
-		
+
 	lookup: function(req, res){
-		
+
 		var term = req.param("term");
-		
+
 		var searchObj = {};
 		//Check if the term has all numbers
 		if(validator.isNumeric(term)){
-			searchObj = { "cin" : {'like': term+'%'}};
+			searchObj = { "type": {"!": ["administrator", "advisor"]}, "cin" : {'like': term+'%'}};
 		}
 		//Check if the term has a "@" in it.
 		else if(term.indexOf("@") > 0){
-			searchObj = {"email_address" : {'like': term+'%'}};
+			searchObj = {"type": {"!": ["administrator", "advisor"]}, "email_address" : {'like': term+'%'}};
 		}
-		//Lastly - search by email, first name, last name 
+		//Lastly - search by email, first name, last name
 		else {
-			searchObj = { or: [{"email_address": {'like': term+'%'}}, 
-			                   {"first_name" : {'like': term+'%'}}, 
+			searchObj = { "type": {"!": ["administrator", "advisor"]}, or: [{"email_address": {'like': term+'%'}},
+			                   {"first_name" : {'like': term+'%'}},
 			                   {"last_name" : {'like': term+'%'}}] };
 		}
-		
+
 		user.find().where(searchObj).exec(function(err, resp){
-			
+
 			if(err){
 				console.log(err);
 			}
 			res.json(resp, 200);
 		});
 	}
-		
-		
+
+
 }
